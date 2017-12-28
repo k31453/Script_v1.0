6c223a3ed3c98ceed872fc92e196eff1367d9154
Kalp Patel <kalp_patel93@yahoo.com>
2017-12-28 16:23:29 +1100

    First project

---
+++ src/sample/Controller.java
@@ -0,0 +1,34 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Controller {
+
+    @FXML
+    private Button uAccount;
+
+    @FXML
+    private Button uDC;
+
+    @FXML
+    void handleButtonAction(ActionEvent event) throws IOException{
+        Parent accountView = FXMLLoader.load(getClass().getResource("Account.fxml"));
+        Scene accountScene = new Scene(accountView);
+        //this line get stage information
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(accountScene);
+        window.show();
+
+    }
+
+}
